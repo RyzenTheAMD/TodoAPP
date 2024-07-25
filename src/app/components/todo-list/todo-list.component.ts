8167829d0d8297fa218f877b0df6b993d73f5638
RyzenTheAMD <karo.hovnanyan@tumo.org>
2024-07-25 13:28:33 +0400

    integrated local storage

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -3,6 +3,7 @@ import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {Todo} from "../../models/todo";
 import {TodoService} from "../../services/todo.service";
 import {find} from "rxjs";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-todo-list',
@@ -11,7 +12,8 @@ import {find} from "rxjs";
   imports: [
     NgForOf,
     NgClass,
-    NgIf
+    NgIf,
+    FormsModule
   ],
   styleUrls: ['./todo-list.component.css']
 })
@@ -23,10 +25,115 @@ export class TodoListComponent implements OnInit {
     'Update documentation',
     'Attend meeting',
     'Send email',
-    'Go to GYM'
+    'Go to GYM',
+    "Finish homework",
+    "Buy groceries",
+    "Call mom",
+    "Go for a run",
+    "Read a book",
+    "Clean the garage",
+    "Cook dinner",
+    "Attend meeting at work",
+    "Practice coding",
+    "Water the plants",
+    "Pay bills",
+    "Do laundry",
+    "Schedule dentist appointment",
+    "Watch a movie",
+    "Write blog post",
+    "Organize closet",
+    "Take out the trash",
+    "Review notes",
+    "Plan weekend getaway",
+    "Learn a new recipe",
+    "Exercise for 30 minutes",
+    "Send thank you cards",
+    "Volunteer at local shelter",
+    "Check email",
+    "Create budget",
+    "Study for exam",
+    "Walk the dog",
+    "Listen to podcast",
+    "Clean out inbox",
+    "Update resume",
+    "Set goals for the month",
+    "Read news",
+    "Shop for birthday gift",
+    "Practice meditation",
+    "Fix leaking faucet",
+    "Research vacation destinations",
+    "Join online course",
+    "Write in journal",
+    "Prepare for presentation",
+    "Call a friend",
+    "Update social media profiles",
+    "Start book club",
+    "Review insurance policies",
+    "Explore new neighborhood",
+    "Attend yoga class",
+    "Check bank balance",
+    "Paint a picture",
+    "Declutter desk",
+    "Create music playlist",
+    "Repair bicycle",
+    "Attend networking event",
+    "Visit library",
+    "Plan home improvement project",
+    "Donate old clothes",
+    "Watch tutorial videos",
+    "Write thank you notes",
+    "Practice a musical instrument",
+    "Research investment options",
+    "Organize digital files",
+    "Try a new restaurant",
+    "Visit museum",
+    "Write poetry",
+    "Update software",
+    "Buy new shoes",
+    "Sort through old photos",
+    "Call customer service",
+    "Plan family gathering",
+    "Attend webinar",
+    "Learn basic first aid",
+    "Read bedtime story to kids",
+    "Practice a foreign language",
+    "Review insurance policies",
+    "Try new workout routine",
+    "Create a budget spreadsheet",
+    "Explore local farmers market",
+    "Set up home office",
+    "Attend local community event",
+    "Visit art gallery",
+    "Research household appliances",
+    "Learn about local history",
+    "Write a short story",
+    "Explore nature trail",
+    "Visit botanical garden",
+    "Attend online workshop",
+    "Try a new hobby",
+    "Create a vision board",
+    "Practice mindfulness",
+    "Visit a new coffee shop",
+    "Read inspirational quotes",
+    "Join a fitness class",
+    "Organize digital photos",
+    "Learn to knit",
+    "Plan next vacation",
+    "Attend virtual conference",
+    "Write a blog",
+    "Research career opportunities",
+    "Try a new skincare routine",
+    "Create a family tree",
+    "Read a biography",
+    "Visit a thrift store",
+    "Plan a surprise",
+    "Attend local theater performance",
+    "Write a song",
+    "Try a DIY project"
   ];
-
   todos: Todo[] = []
+  onGoingEdit: boolean = false;
+
 
   placeholder: string = '';
   getRandomTask(): string {
@@ -39,14 +146,19 @@ export class TodoListComponent implements OnInit {
     this.fetchTodos()
   }
 
+
   constructor(private todoService: TodoService) {
   }
 
-  OnAdd(value: string) {
+  OnAdd(inputElement: HTMLInputElement) {
+    const value = inputElement.value;
+    if(value.length == 0 ) return
     const todo = new Todo('-1', value, false, false)
     const newTodo = this.todoService.create(todo);
-    this.todos.push(newTodo)
+    this.todos.unshift(newTodo)
+    inputElement.value = ''
   }
+
   OnDelete(id: string) {
     if (confirm('Are you sure you want to delete it?')) {
       const DeletedTodo = this.todoService.delete(id)
@@ -62,26 +174,35 @@ export class TodoListComponent implements OnInit {
   }
 
   OnEdit(todo: Todo) {
-    todo.isEditing = true
+    if(!this.onGoingEdit) {
+      todo.isEditing = true
+    }
+    this.onGoingEdit = true
   }
 
   OnSave(todo: Todo, title : string) {
     todo.title = title
     todo.isEditing = false
-    const updatedTodo = this.todoService.update(todo)
-    if(updatedTodo){
-      const index = this.todos.findIndex(item => item.id === updatedTodo.id);
-      this.todos[index] = {...updatedTodo}
-    }
+    this.updateTodo(todo)
+    this.onGoingEdit = false
   }
 
   OnCancel(todo: Todo) {
     todo.isEditing = false
     const index = this.todos.findIndex(item => item.id === todo.id);
     this.todos[index] = {...this.todos[index]}
+    this.onGoingEdit = false
   }
 
-  private findById(id: string): Todo | undefined {
-    return this.todos.find(todo => todo.id === id)
+  onDoneChange(todo: Todo) {
+    this.updateTodo(todo)
+  }
+
+  private updateTodo(todo: Todo) {
+    const updatedTodo = this.todoService.update(todo)
+    if(updatedTodo){
+      const index = this.todos.findIndex(item => item.id === updatedTodo.id);
+      this.todos[index] = {...updatedTodo}
+    }
   }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import {Todo} from "../models/todo";
+import {find} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
   todos = [] as Todo[];
+
   constructor() { }
 
-  getAll(){
+  getAll(): Todo[]{
+    this.todos = JSON.parse(localStorage.getItem('todo-items') || '[]') as Todo[];
     return [...this.todos];
   }
   create(todo: Todo) : Todo{
     const id = new Date().getTime().toString();
     const updatedTodo = {...todo, id: id};
-    this.todos.push(updatedTodo);
+    this.todos.unshift(updatedTodo);
+    this.setAll(this.todos);
     return updatedTodo;
   }
 
@@ -22,12 +26,18 @@ export class TodoService {
     const index = this.todos.findIndex(todo => todo.id === id);
     const deletedTodo = this.todos[index]
     this.todos.splice(index, 1);
+    this.setAll(this.todos)
     return deletedTodo.id
   }
 
   update(todo: Todo) {
     const index = this.todos.findIndex(item => item.id === todo.id);
     this.todos[index] = {...todo};
+    this.setAll(this.todos);
     return this.todos[index]
+  }
+
+  private setAll(todos: Todo[]) {
+    localStorage.setItem('todo-items', JSON.stringify(todos));
   }
 }
